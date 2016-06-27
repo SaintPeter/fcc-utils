d33b57edba16ec1fc972462f764ebd7a88e65271
Rex Schrader <rex.schrader@gmail.com>
2016-06-27 10:57:01 -0700

    Make human readable

--- challenge_count.js
+++ challenge_count.js
@@ -4,6 +4,7 @@ var path = '../FreeCodeCamp/seed/challenges/';
 
 var fileCount = 0;
 var counts = {};
+var total = 0;
 
 var superblocks = fs.readdirSync(path);
 
@@ -14,6 +15,8 @@ superblocks.forEach(block => {
   counts[block].total = 0;
   counts[block].required_total = 0;
 
+  console.log("Block:" + block);
+
   challengeFiles.forEach(file => {
     var changed = false;
     var count = 0;
@@ -22,13 +25,15 @@ superblocks.forEach(block => {
     var fileData = fs.readJsonSync(filename);
 
     counts[block][file] = {};
-
     counts[block][file].total = fileData.challenges.length;
     counts[block][file].required = fileData.challenges.filter(item => item.isRequired).length;
     counts[block][file].optional = fileData.challenges.filter(item => !item.isRequired).length;
     counts[block].total += fileData.challenges.length;
+    total += fileData.challenges.length;
     counts[block].required_total += counts[block][file].required;
-  });
-});
 
-console.log(counts);
+    console.log("\tTitle: " + fileData.name + " (" + fileData.challenges.length + ")");
+  });
+  console.log("\tBlock Total: " + counts[block].total);
+});
+console.log("Overall Total: " + total);
